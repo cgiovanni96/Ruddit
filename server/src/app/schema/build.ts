28756988305fcbf276e8fdc3af45033e24b8eee3
Giovanni Costa <cgiovanni1996@gmail.com>
2021-01-05 18:18:46 +0100

    server: :bug: fixed an error in the schema emit path

--- server/src/app/schema/build.ts
+++ server/src/app/schema/build.ts
@@ -1,10 +1,13 @@
 import { GraphQLSchema } from 'graphql'
 import { buildSchema } from 'type-graphql'
+import path from 'path'
 
 import resolvers from './resolvers'
 
+const schemaPath = path.join(__dirname, 'schema.graphql')
+
 export default async (emitSchema = false): Promise<GraphQLSchema> => {
-	const emitSchemaFile = emitSchema ? './schema.graphql' : false
+	const emitSchemaFile = emitSchema ? schemaPath : false
 
 	return await buildSchema({
 		resolvers,
