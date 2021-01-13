f77dd8757c6d9ed7e1b43a66b9fd5070082877ee
Giovanni Costa <cgiovanni1996@gmail.com>
2021-01-13 09:27:34 +0100

    server: :sparkles: improving server side voting

--- server/src/resolver/vote/index.ts
+++ server/src/resolver/vote/index.ts
@@ -13,21 +13,46 @@ export default class VoteResolver {
 		@Arg('value', () => Int) value: number,
 		@Ctx() { req }: Context
 	): Promise<boolean> {
+		const { userId } = req.session
+		const vote = await Vote.findOne({ where: { userId, postId } })
+
 		const isPositive = value !== -1
 		const escapedValue = isPositive ? 1 : -1
-		const { userId } = req.session
 
-		await Vote.insert({
-			userId,
-			postId,
-			value: escapedValue
-		})
+		// if voted a second time, I need to do (2 * value),
+		// -1 => 1, or 1 => -1
+		let factoredValue: number
+
+		// first time voting
+		if (!vote) {
+			factoredValue = escapedValue
+			await Vote.insert({
+				userId,
+				postId,
+				value: factoredValue
+			})
+		}
+
+		// changing the vote (upvote => downvote || downvote => upvote)
+		if (vote && vote.value !== escapedValue) {
+			factoredValue = 2 * escapedValue
+			await Vote.update(
+				{
+					userId,
+					postId
+				},
+				{ value: factoredValue }
+			)
+		}
+
+		// same vote, no need to perform the query
+		if (vote && vote.value === escapedValue) return false
 
 		await getConnection()
 			.createQueryBuilder()
 			.update(Post)
 			.where('id = :id', { id: postId })
-			.set({ points: () => `points + ${escapedValue}` })
+			.set({ points: () => `points + ${factoredValue}` })
 			.execute()
 
 		return true
