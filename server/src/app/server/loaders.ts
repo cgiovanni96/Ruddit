54d22fdab17550c4298b8926b067d30e1402f353
Giovanni Costa <cgiovanni1996@gmail.com>
2021-02-02 15:16:26 +0100

    server: :sparkles: comment by user ids loader

--- server/src/app/server/loaders.ts
+++ server/src/app/server/loaders.ts
@@ -2,19 +2,22 @@ import voteLoader from '../loader/voteLoader'
 import userLoader from '../loader/userLoader'
 import subrudditLoader from '../loader/subrudditLoader'
 import commentByPostIdsLoader from '../loader/commentByPostIdsLoader'
+import commentByUserIdsLoader from '../loader/commentByUserIdsLoader'
 
 export type Loaders = {
 	userLoader: ReturnType<typeof userLoader>
 	voteLoader: ReturnType<typeof voteLoader>
 	subrudditLoader: ReturnType<typeof subrudditLoader>
 	commentByPostIdsLoader: ReturnType<typeof commentByPostIdsLoader>
+	commentByUserIdsLoader: ReturnType<typeof commentByUserIdsLoader>
 }
 
 export const loaders: Loaders = {
 	userLoader: userLoader(),
 	voteLoader: voteLoader(),
 	subrudditLoader: subrudditLoader(),
-	commentByPostIdsLoader: commentByPostIdsLoader()
+	commentByPostIdsLoader: commentByPostIdsLoader(),
+	commentByUserIdsLoader: commentByUserIdsLoader()
 }
 
 export default Loaders
